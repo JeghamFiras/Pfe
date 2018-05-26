89db46c00d7bf8546bcc30e06f3670f36e17e597
root <root@6cea0f54ee0b>
2018-05-26 23:16:43 +0000

    demopfe

--- deploiement-script.sh
+++ deploiement-script.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ssh -i "continuous-integration.pem" ubuntu@ec2-52-211-182-147.eu-west-1.compute.amazonaws.com "cd /home/ubuntu; sudo rm -r microservices-simple ;mkdir microservices-simple; cd microservices-simple;git init;git pull https://github.com/JeghamFiras/Pfe;cd web-application;ng build;cd ..;mvn clean package;cd build/docker; sudo export profile=docker;sudo docker-compose up --build -d"
+ssh -i "continuous-integration.pem" ubuntu@ec2-52-211-182-147.eu-west-1.compute.amazonaws.com "cd /home/ubuntu; sudo rm -r microservices-simple ;mkdir microservices-simple; cd microservices-simple;git init;git pull https://github.com/JeghamFiras/Pfe;cd web-application;ng build;cd ..;mvn clean package;cd build/docker;export profile=docker;sudo docker-compose up --build -d"
